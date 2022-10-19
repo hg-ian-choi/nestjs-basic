3370878542538d9cb097a667ae17b17b57a68851
hg-ian-choi <hg.ian.choi@gmail.com>
2022-10-19 21:51:53 +0900

    try more e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -40,18 +40,25 @@ describe('AppController (e2e)', () => {
   });
 
   describe('/movies', () => {
-    it('/ (GET)', () => {
+    it('/ (GET) 200', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('/ (POST)', () => {
+    it('/ (POST) 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({ title: 'Test', year: 2000, genres: ['test'] })
         .expect(201);
     });
 
-    it('/ (DELETE)', () => {
+    it('/ (POST) 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({ title: 'Test', year: 2000, genres: ['test'], wrong: 'wrong' })
+        .expect(400);
+    });
+
+    it('/ (DELETE) 404', () => {
       return request(app.getHttpServer()).delete('/999').expect(404);
     });
   });
@@ -61,7 +68,22 @@ describe('AppController (e2e)', () => {
     it('GET 200', () => {
       return request(app.getHttpServer()).get('/movies/1').expect(200);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+
+    // it.todo('PATCH');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+
+    // it.todo('DELETE');
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
